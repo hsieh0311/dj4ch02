13fe85bde1eb39f0e93cd14c5492214efa456f0f
meat <hsiehxinyu0311@gmail.com>
2024-04-05 22:17:19 +0800

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from mysite.models import Post
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+def homepage(request):
+    posts = Post.objects.sll()
+    now=datetime.now()
+    return render(request,"index.html",locals())
